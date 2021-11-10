3cf3e0b20f6468b08da39573f668601aa080c84b
Matt Searle <matthewsearle01@gmail.com>
2021-11-10 10:15:43 +0000

    fixed responsiveness

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require "vendor/autoload.php";
 use Transformers\viewhelpers\IndexViewHelper;
 
 $indexViewHelper = new IndexViewHelper();
-$indexViewHelper->searchTransformers('Gr');
+// $indexViewHelper->searchTransformers('Gr');
 
 ?>
 
