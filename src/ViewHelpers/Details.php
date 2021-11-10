0ff6d44f75d4bb812e38128c4d79315162e6f7c4
Matt Searle <matthewsearle01@gmail.com>
2021-11-10 20:24:23 +0000

    final css

--- src/ViewHelpers/Details.php
+++ src/ViewHelpers/Details.php
@@ -41,22 +41,22 @@ class Details
         $type = $this->transformer->type;
         $img_url = $this->transformer->img_url;
 
-        $str = '<div class="m-xl-5 m-4">
-                    <img class="transformer-details-image" src="$img_url" alt="picture of $name" />
+        $str = "<div class='m-xl-5 m-4'>
+                    <img class='transformer-details-image' src='$img_url' alt='picture of $name' />
                 </div>
-                <div class="transformer-details mx-5">
-                    <ul class="display-4 list-unstyled">
+                <div class='transformer-details mx-5'>
+                    <ul class='display-4 list-unstyled'>
                         <li>$name</li>
-                        <li class="display-6">$type</li>
+                        <li class='display-6'>$type</li>
                     </ul>
-                    <ul class="list-unstyled fs-4">
+                    <ul class='list-unstyled fs-4'>
                         <li>Size: $size</li>
                         <li>Speed: $speed</li>
                         <li>Power: $power</li>
                         <li>Disguise: $disguise</li>
                         <li>Top Trumps Rating: $top_trumps_rating</li>
                     </ul>
-                </div>';
+                </div>";
 
         return $str;
     }
