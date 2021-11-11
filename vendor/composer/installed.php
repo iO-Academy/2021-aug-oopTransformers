b858762771f27dcc85b682194c2453bf65a7afa3
Henri <henridvf@gmail.com>
2021-11-11 11:17:41 +0000

    Restored vendor files for auto-load

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -5,7 +5,7 @@
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => '3cf3e0b20f6468b08da39573f668601aa080c84b',
+        'reference' => '44e70d2557f98e1b9cadc704f5c3b2d1b56d16c2',
         'name' => '__root__',
         'dev' => true,
     ),
@@ -16,7 +16,7 @@
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => '3cf3e0b20f6468b08da39573f668601aa080c84b',
+            'reference' => '44e70d2557f98e1b9cadc704f5c3b2d1b56d16c2',
             'dev_requirement' => false,
         ),
         'doctrine/instantiator' => array(
