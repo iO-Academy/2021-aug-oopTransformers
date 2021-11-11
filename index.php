ca9de6e1e8e0c496e3db7129399df05f13dba191
Matt Searle <matthewsearle01@gmail.com>
2021-11-11 16:51:39 +0000

    added title to html

--- index.php
+++ index.php
@@ -27,6 +27,7 @@ if (isset($_GET['search'])) {
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <link rel="stylesheet" href="css/styles.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css">
+        <title>Transformers</title>
     </head>
     <body>
         <main class="d-flex flex-column flex-wrap" id="homePage">
