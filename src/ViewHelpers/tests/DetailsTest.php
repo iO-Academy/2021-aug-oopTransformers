9d485a674fbf56c1078e11e34e24eafe4244f161
Ben <benjamindhayden@gmail.com>
2021-11-11 10:20:02 +0000

    DetailsModel.php unit testing.

---
+++ src/ViewHelpers/tests/DetailsTest.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+require_once '../Details.php';
+use Transformers\ViewHelpers\Details;
+require_once '../../Abstracts/Transformer.php';
+use Transformers\Abstracts\Transformer;
+use PHPUnit\Framework\TestCase;
+
+
+class DetailsModelTest extends TestCase
+{
+    
+}
